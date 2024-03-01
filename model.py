b8ff529903b95a66b42f41efb5e3e0e8b0cd3daa
AsphodelRem <asphodel.bog.rem@gmail.com>
2024-03-01 12:59:17 +0000

    Fix mistake in a forward method

--- model.py
+++ model.py
@@ -1,20 +1,19 @@
 # Models
 
-import torch
-import torch.nn a nn
-
+import torch.nn.functional as F
+import torch.nn as nn
 
 class BaselineModel(nn.Module):
-    """
-    Nasseri, M., Pal Attia, T., Joseph, B. et al. 
-    Ambulatory seizure forecasting with a wrist-worn device using long-short term memory deep learning. 
-    Sci Rep 11, 21935 (2021). https://doi.org/10.1038/s41598-021-01449-2
-    """
     def __init__(self, input_size: int, hidden_nodes: int=128, lstm_layers: int=4):
-        super(TestModel, self).__init__()
-        self.rnn = nn.LSTM(input_size, lstm_layers, hidden_nodes, dropout=0.2)
-        self.fc = nn.Linear(hidden_nodes * lstm_layers, 2)
+        super(BaselineModel, self).__init__()
+        self.rnn = nn.LSTM(input_size, hidden_nodes, lstm_layers, dropout=0.2, batch_first=True)
+        self.fc = nn.Linear(hidden_nodes, 2)  
         self.sigmoid = nn.Sigmoid()
-        
+
     def forward(self, x):
-        return self.sigmoid(self.fc(self.rnn(x)))
+        lstm_out, _ = self.rnn(x)
+        lstm_out = lstm_out[:, -1, :]
+        output = self.sigmoid(self.fc(lstm_out))
+        
+        return output
+    
